205a51a45b078ccd5ebdd2a44654747ecc529c79
Celsolf <71788045+Celsolf@users.noreply.github.com>
2024-10-22 15:24:32 -0300

    creation of models

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Candidato(models.Model):
+    candidato_nome = models.CharField(max_length=150, default='')
+    candidato_nascimento =models.DateField((""), auto_now=False, auto_now_add=False)
+    candidato_cpf =models.IntegerField(default=0)
+
+    candidato_email =models.EmailField(default=0)
+    candidato_telefone =models.IntegerField(default=0)
+    candidato_endereco =models.CharField(max_length=150, default='')
+    candidato_cargo =models.CharField(max_length=150, default='')
+    candidato_empresa =models.CharField(max_length=150, default='')
+    candidato_periodo =models.CharField(max_length=150, default='')
+
+    candidato_descricao =models.CharField(max_length=350, default='')
+    candidato_instituicao =models.CharField(max_length=150, default='')
+    candidato_curso =models.CharField(max_length=150, default='')
+    candidato_periodo =models.CharField(max_length=150, default='')
+
+    def __str__(self):
+        return f'Nome: {self.candidato_nome} | E-mail:{self.candidato_email}'
+
