e182860e0bec1c0d7e8ebaf035892bf339ffc9b1
Celsolf <71788045+Celsolf@users.noreply.github.com>
2024-10-22 16:02:25 -0300

    creating first urls

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Candidato
+from .serializers import CandidatoSerializer
+
+import json
+
+
+@api_view(['GET'])
+def get_candidatos(request):
+
+    if request.method == 'GET':
+
+        candidatos = Candidato.objects.all()
+        serializer = CandidatoSerializer(candidatos, many=True)
+
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
